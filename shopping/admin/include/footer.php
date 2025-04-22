1f1a34967a5098e53087d435f0c5d325ff270406
hopnesscrew <hopnesscrew@gmail.com>
2025-04-22 12:18:09 +0000

    first commit

---
+++ shopping/admin/include/footer.php
@@ -0,0 +1,7 @@
+	<div class="footer">
+		<div class="container">
+			 
+
+			<b class="copyright">&copy; Plantopia admin side </b> All Setting can change by admin
+		</div>
+	</div>
---
+++ shopping/includes/brands-slider.php
@@ -0,0 +1,41 @@
+<div id="brands-carousel" class="logo-slider wow fadeInUp">
+		<center><h3 class="section-title">Our Brands</h3></center>
+		<div class="logo-slider-inner">	
+			<div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/plant1.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="https://greenovation.pk/" class="image">
+						<img data-echo="brandsimage/plant2.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+				<div class="item">
+					<a href="https://greenovation.pk/" class="image">
+						<img data-echo="brandsimage/plant3.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="https://greenovation.pk/" class="image">
+						<img data-echo="brandsimage/plant4.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="https://greenovation.pk/" class="image">
+						<img data-echo="brandsimage/plant6.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+					</div><!--/.item-->
+				<div class="item">
+					<a href="https://greenovation.pk/" class="image">
+						<img data-echo="brandsimage/plant3.jpg" src="assets/images/blank.gif" alt="">
+					</a>
+				</div><!--/.item-->
+		    </div><!-- /.owl-carousel #logo-slider -->
+		</div><!-- /.logo-slider-inner -->
+	
+</div><!-- /.logo-slider -->
